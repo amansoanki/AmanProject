11a9157cfb96ca03ab8ddec613f9fffe4e3bf9c0
amansoanki <161851609+amansoanki@users.noreply.github.com>
2024-03-22 11:43:49 +0530

    Add files via upload

---
+++ ipl.js
@@ -0,0 +1,13 @@
+<script>
+
+    function myFun(image, colour, head, logo){
+        document.getElementById('myimage').src=image;
+        document.querySelector('.empty').style.backgroundColor=colour;
+        document.querySelector('button').style.backgroundColor=colour;
+        document.getElementById('head').innerHTML=head;
+        document.getElementById('logobg').src=logo;
+    }
+
+
+</script>
+
